701872424efa1cbced08b310f85bdde52411760b
Lombial <148324411+Lombial@users.noreply.github.com>
2023-11-16 00:45:42 +0300

    ()

--- Renamer/CommandResearch.cs
+++ Renamer/CommandResearch.cs
@@ -25,5 +25,14 @@ namespace Renamer
 
             else Tools.Rename(args.Parameters[1], args.Parameters[2], args);
         }
+        public static void Username(CommandArgs args)
+        {
+            if (args.Parameters.Count != 2) args.Player.SendErrorMessage("Invalid user syntax. Proper syntax: {0}username <username> <new_name>.", TShock.Config.Settings.CommandSpecifier);
+            else if (Tools.VerifyName(args.Parameters[0])) args.Player.SendErrorMessage("User {0} does not exist.", args.Parameters[0]);
+            else if (args.Parameters[0] == args.Parameters[1]) args.Player.SendErrorMessage("You can not use account's current name to change the name.");
+            else if (!Tools.VerifyName(args.Parameters[1])) args.Player.SendErrorMessage("Sorry, {0} was already taken by another person.", args.Parameters[1]);
+
+            else Tools.Rename(args.Parameters[0], args.Parameters[1], args);
+        }
     }
 }
--- Renamer/ThePlugin.cs
+++ Renamer/ThePlugin.cs
@@ -1,4 +1,5 @@
 ﻿using TShockAPI;
+using TShockAPI.Configuration;
 using Terraria;
 using TerrariaApi.Server;
 
@@ -8,27 +9,44 @@ namespace Renamer
     public class ThePlugin : TerrariaPlugin
     {
         public override string Name => "Renamer";
-        public override string Description => "Allows you to change user's account name";
+        public override string Description => "Allows you to change user's account name.";
         public override Version Version => new Version(0, 0, 1);
         public override string Author => "Lombial";
+        public static ConfigFile<Settings> Config { get; private set; }
         public ThePlugin(Main game) : base(game) { }
         public override void Initialize()
         {
+            string path = Path.Combine(TShock.SavePath, "Renamer.json");
+            Config = new();
+            Config.Read(path, out bool write);
+            if (write)
+                Config.Write(path);
             Action<Command> add = arg =>
             {
                 Commands.ChatCommands.RemoveAll(arg1 => arg1.Names.Exists(arg2 => arg.Names.Contains(arg2)));
                 Commands.ChatCommands.Add(arg);
             };
+            if (Config.Settings.TurnOnUserCommandExtension)
+                add(new Command(Permissions.user, CommandResearch.Manageusers, "user")
+                {
+                    DoLog = false,
+                    HelpText = "Manages user accounts."
+                });
+            else
+                add(new Command("renamer.admin", CommandResearch.Username, "username")
+                {
+                    HelpText = "Manages user account name."
+                });
             add(new Command("renamer.self", CommandResearch.RenameCommand, "changename", "cname")
             {
                 AllowServer = false,
                 HelpText = "Allows you to change your account name."
             });
-            add(new Command(Permissions.user, CommandResearch.Manageusers, "user") 
-            {
-                DoLog = false,
-                HelpText = "Manages user accounts."
-            });
         }
+    }
+    public class Settings
+    {
+        public bool TurnOnUserCommandExtension = true;
+
     }
 }
